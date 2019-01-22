22831c1ccec0930f37fded137dc8c4a8d3913271
S-Hucks <sh473917@ohio.edu>
2019-01-22 10:49:11 -0500

    Initial Commit

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+//*********************************************************
+//
+//   File: git-lab-program.cc
+//   Author: Sebastien Hucks
+//   Date: January 22, 2019
+//   Description: A program that writes something to the screen
+//
+//*********************************************************
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+int main(int argc, char const *argv[]){
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
